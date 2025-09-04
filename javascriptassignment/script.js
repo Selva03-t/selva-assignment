2aa8327295f2a808afa04adb2ec278a3543243cc
Selva03-t <selvaganapathi03t@gmail.com>
2025-09-04 10:22:58 +0530

    JS
    javascript assignment

---
+++ javascriptassignment/script.js
@@ -0,0 +1,49 @@
+const a = 20
+const b = 30
+const c = 10
+
+console.log("Addition:", a + b);        
+console.log("Subtraction:", a - b);     
+console.log("Multiplication:", a * b);  
+console.log("Division:", a / b);        
+console.log("Modulus:", a % b);         
+console.log("Exponentiation:", a ** b); 
+console.log("Remainder:", a % b); 
+let result = Math.floor(a / b);
+console.log("Floor division:", result);
+
+
+
+console.log( a == b);   
+console.log( a === b); 
+console.log( a != c);   
+console.log( a !== b); 
+console.log( c > a);     
+console.log( a < c);   
+console.log( a >= 10);  
+console.log( c <= 20);  
+
+let p = true;
+let q = false;
+console.log( p && q); 
+console.log( p || q);  
+console.log( !p);     
+
+let x = 40;
+x += 5;
+console.log(x);
+x -= 20;
+console.log(x);
+x *= 3;
+console.log(x);
+x /= 4;
+console.log(x);
+x %= 4;
+console.log(x);
+x **= 7;
+console.log(x);
+
+
+
+
+
